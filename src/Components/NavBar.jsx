9d5c4e246ab4d0a01b5b817017ce90e46b49b3a1
Priyanka1g <71247332+Priyanka1g@users.noreply.github.com>
2022-02-02 14:54:46 +0530

    Create NavBar.jsx

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import "./NavBar.css"
+const Navbar =() =>{
+    return (
+        <div>
+           <header className="header">
+           <h1>WELCOME TO MOVIE APP</h1>
+           </header> 
+           </div>
+    )
+}
+
+export default Navbar
